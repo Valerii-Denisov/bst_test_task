c8defbf7c7c49b5876bfac0589b74af179904698
Valerii-Denisov <klerk919191@gmail.com>
2023-09-26 16:09:20 +0300

    realise logic to signals and send email

--- api_app/admin.py
+++ api_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import OrdersQueue
+
+admin.site.register(OrdersQueue)
 
 # Register your models here.
--- api_app/models.py
+++ api_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from orders.models import Order
 
 # Create your models here.
+class OrdersQueue(models.Model):
+    STATUS_OF_ORDER = [
+        ('Done', 'Order is done'),
+        ('Wait', 'Customer is waiting his robot'),
+    ]
+    order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=4,
+        choices=STATUS_OF_ORDER,
+        default='Wait',
+    )
